79cdf3c0ef59769384615b2c41511211f50aa7c8
moooe <37929999+moooe@users.noreply.github.com>
2018-05-05 00:03:56 +0900

    ・道の自動生成 ・主人公の走るとジャンプのアニメーション ・上キーでジャンプ ・アイテムのキャンディー、石の配置
    出来たのはここまでです。
    今週ずっと体調がすぐれず、パソコンに向かう時間が少ないなかで取り組んだ結果です。申し訳ございません。
    唯一の出来る日に熱がでてしまい、時間を確実に確保出来ませんでした。寝て体調を治し、明日の勉強会に出来るだけ参加して頑張って進めます。
    Day5で締め切りのことを話されたばかりなのに本当に申し訳ございません。

---
+++ Assets/CreateScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateScript : MonoBehaviour {
+
+	public GameObject course1;
+	public GameObject course2;
+
+	int border = 5;
+
+	void Update (){
+		if (transform.position.z > border) {
+			CreateMap ();
+		}
+	}
+
+	void CreateMap(){
+		if (course1.transform.position.z < border) {
+			border += 30;
+			Vector3 temp = new Vector3 (0,-0.49f,border);
+			course1.transform.position = temp;
+		} else if (course2.transform.position.z < border) {
+			border += 30;
+			Vector3 temp = new Vector3 (0,-0.49f,border);
+			course2.transform.position = temp;
+		}
+	}
+
+	// Use this for initialization
+	
+}
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float speed = 3.0f;
+	public float slidspeed = 1.5f;
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent <Animator> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += Vector3.forward * speed * Time.deltaTime;
+		float pos_x = transform.position.x;
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			if (pos_x > -1.9f) {
+				transform.position += Vector3.left * slidspeed * Time.deltaTime;
+			}
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			if (pos_x < 1.9f) {
+				transform.position += Vector3.right * slidspeed * Time.deltaTime;
+			}
+		}
+
+
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			animator.SetBool ("JUMP", true);
+			animator.SetBool ("JUMP", true);
+		}
+		if (Input.GetKeyUp (KeyCode.UpArrow)) {
+			animator.SetBool ("JUMP", false);
+			animator.SetBool ("RUN", true);
+		}
+		
+	}
+		
+		
+	
+}
